4362ccf0d8438e18596224146e480729f23476cd
esraa69 <esraa.abdelmonaim@dev-chef.com>
2020-04-27 18:26:37 +0200

    route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule, Routes } from '@angular/router'
+import { createComponent } from '@angular/compiler/src/core';
+
 import { CreateProductComponent } from './Components/create-product/create-product.component';
 import { EditProductComponent } from './Components/edit-product/edit-product.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import{RouterModule, Routes} from '@angular/router'
 import { ProductsListComponent } from './Components/products-list/products-list.component';
 import { ProductComponent } from './Components/product/product.component';
 import { OrderComponent } from './components/order/order.component';
 import { OrderListComponent } from './components/order-list/order-list.component';
 
-const routes:Routes=[
-  {path:""},
-  {path:"admin/products",component:ProductsListComponent},
-  {path:"admin/product",component:ProductComponent},
+const routes: Routes = [
+
+  { path: "admin/products", component: ProductsListComponent },
+  { path: "admin/orders", component: OrderListComponent },
+  { path: 'admin/create', component: CreateProductComponent },
+  { path: 'admin/edit', component: EditProductComponent }
 ]
 
 
@@ -24,12 +27,14 @@ const routes:Routes=[
     CreateProductComponent,
     EditProductComponent,
     ProductComponent,
+    ProductsListComponent,
     OrderComponent,
     OrderListComponent
   ],
   imports: [
     BrowserModule,
-    NgbModule
+    NgbModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
