5293bf2ddbf378fee20b538ed334abe57f3f49e6
esraa69 <esraa.abdelmonaim@dev-chef.com>
2020-04-27 17:14:41 +0200

    Editproduct

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CreateProductComponent } from './Components/create-product/create-product.component';
+import { EditProductComponent } from './Components/edit-product/edit-product.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import{RouterModule, Routes} from '@angular/router'
+import { ProductsListComponent } from './Components/products-list/products-list.component';
+import { ProductComponent } from './Components/product/product.component';
+
+
+const routes:Routes=[
+  {path:""},
+  {path:"admin/products",component:ProductsListComponent},
+  {path:"admin/product",component:ProductComponent},
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    CreateProductComponent
+    CreateProductComponent,
+    EditProductComponent,
+    ProductComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
