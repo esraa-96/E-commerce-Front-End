92107dcec219ab439699aaa2057a1924e6bd872f
aosamaph <32076967+aosamaph@users.noreply.github.com>
2020-05-14 12:45:50 +0200

    hosting trial 5

--- server.js
+++ server.js
@@ -1,23 +1,6 @@
-"use strict";
-const express = require("express");
-const compression = require("compression");
-
-const _port = 4100;
-const _app_folder = 'dist/application';
-
+const express = require('express');
 const app = express();
-app.use(compression());
 
+app.use(express.static(__dirname + '/dist/firstProject'));
 
-// ---- SERVE STATIC FILES ---- //
-app.server.get('*.*', express.static(_app_folder, { maxAge: '1y' }));
-
-// ---- SERVE APLICATION PATHS ---- //
-app.all('*', function (req, res) {
-    res.status(200).sendFile(`/`, { root: _app_folder });
-});
-
-// ---- START UP THE NODE SERVER  ----
-app.listen(_port, function () {
-    console.log("Node Express server for " + app.name + " listening on http://localhost:" + _port);
-});
+app.listen(process.env.PORT || 8080);
